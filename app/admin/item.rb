694d280faecd4ae7af11f1c5230eabb978b5d90e
Dmitrii <wild_dima@mail.ru>
2015-06-12 19:11:57 +0300

    Настроил carrierwave в activeadmin

--- app/admin/item.rb
+++ app/admin/item.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register Item do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
- permit_params :name, :description, :price
+permit_params :name, :description, :price, :image
 #
 # or
 #
@@ -12,6 +12,14 @@ ActiveAdmin.register Item do
 #   permitted << :other if resource.something?
 #   permitted
 # end
-
+index do
+	column :name
+	column :description
+	column :price
+	column :image do |item|
+		image_tag item.image.url, size: '100x100'
+	end
+	actions
+end
 
 end
